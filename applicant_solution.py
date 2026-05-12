6a8c9512352fb7a1cdf69b32857d0f8fbe730684
raffihyouka-afk <raffihyouka@gmail.com>
2026-05-12 22:20:42 +1000

    Update applicant_solution.py

--- applicant_solution.py
+++ applicant_solution.py
@@ -21,59 +21,19 @@ helpers = build_task_helpers(tx_n, Fs, N)
 
 
 def your_canceller(tx_n, rx):
-    N_samples, n_tx = tx_n.shape
-    N_samples2, n_rx = rx.shape
+    # tx interference is already modeled in the baseline helper
+    tx_pred = helpers["fit_tx_prediction"](rx)
+    rx_after = rx - tx_pred
     
-    predicted_interference = np.zeros_like(rx)
-    
-    for c in range(n_rx):
-        feat_list = []
-        
-        # linear
-        for i in range(n_tx):
-            feat_list.append(tx_n[:, i].real)
-            feat_list.append(tx_n[:, i].imag)
-        
-        # |tx|^2
-        for i in range(n_tx):
-            feat_list.append(np.abs(tx_n[:, i]) ** 2)
-        
-        # cross products
-        for i in range(n_tx):
-            for j in range(i+1, n_tx):
-                cross = tx_n[:, i] * np.conj(tx_n[:, j])
-                feat_list.append(cross.real)
-                feat_list.append(cross.imag)
-                cross2 = tx_n[:, i] * tx_n[:, j]
-                feat_list.append(cross2.real)
-                feat_list.append(cross2.imag)
-        
-        # squared
-        for i in range(n_tx):
-            sq = tx_n[:, i] ** 2
-            feat_list.append(sq.real)
-            feat_list.append(sq.imag)
-        
-        X = np.column_stack(feat_list)
-        y = rx[:, c]
-        
-        coeffs, residuals, rank, s = np.linalg.lstsq(X, y, rcond=None)
-        pred = X @ coeffs
-        predicted_interference[:, c] = pred
-    
-    rx_after = rx - predicted_interference
-    
-    # external interference - spatially coherent = rank 1?
+    # external interference is spatially coherent = rank 1?
     leftover = rx_after.copy()
     U, S, Vh = np.linalg.svd(leftover, full_matrices=False)
     
-    # not sure about this formula but it runs
+    # not sure if this reconstruction is right but it runs
     rank1 = np.outer(U[:, 0] * S[0], Vh[0, :])
     
-    # 1.0 was worse
+    # 1.0 was worse, 0.7 seems best
     rx_hat = rx_after - 0.7 * rank1
-    
-    # rx_hat = rx_hat + 0.1 * predicted_interference  # tried, didnt help
     
     return rx_hat
 
